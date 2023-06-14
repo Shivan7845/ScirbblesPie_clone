dde803ad85e99e57a49a37fe044ed0af5c19b67c
shivan <shivamn322@gmail.com>
2023-06-14 10:45:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,111 @@
+gsap.from(".nav-part2 h4" ,{
+    y:-100,
+    opacity : 0,
+    duration :1
+}) 
+
+gsap.from(".nav-part1 i",{
+    y: -100,
+    opacity:0,
+    stagger : 0.4,
+    duration : 1,
+    delay:0.5,
+})
+gsap.from(".nav-part3 button" , {
+    x :300,
+    opacity:0,
+    delay :1,
+    duration:1,
+})
+
+gsap.from(".p1-right h1", {
+    x :300,
+    opacity :0,
+    duration: 1.5,
+    delay :1,
+})
+gsap.from(".p1-right button" ,{
+    x :300,
+    opacity:0,
+    delay :1,
+    duration:1,
+})
+gsap.from(".page2 h2" ,{
+    y:-100,
+    delay :3,
+    duration :1,
+    opacity :0,
+})
+gsap.from(".p2-bottom img" ,{
+    y:-100,
+    delay :3,
+    duration :1,
+    opacity :0,
+})
+gsap.from(".p3-left h1 " ,{
+    x:-100,
+    delay :3,
+    duration :4,
+    opacity :0,
+})
+gsap.from(".p3-left p" ,{
+    x:-100,
+    delay :4,
+    duration :1,
+    opacity :0,
+})
+gsap.from(".p3-left button" ,{
+    x:-100,
+    delay :4,
+    duration :1,
+    opacity :0,
+})
+gsap.from(".p3-right" ,{
+    y:100,
+    delay :4,
+    duration :1,
+    opacity :0,
+})
+gsap.from(".p4-content h1" ,{
+    y:-100,
+    delay :6,
+    duration :1,
+    opacity :0,
+})
+gsap.from(".p4-content p" ,{
+    y:100,
+    delay :6,
+    duration :1,
+    opacity :0,
+})
+gsap.from(".p4-content h3" ,{
+    x:-100,
+    delay :6,
+    duration :1,
+    opacity :0,
+})
+
+gsap.from(".p4-content h1" ,{
+    y:-100,
+    delay :5,
+    duration :1,
+    opacity :0,
+})
+gsap.from(".p5c-left h1" ,{
+    y:-100,
+    delay :7,
+    duration :1,
+    opacity :0,
+})
+gsap.from(".p5c-left h3" ,{
+    y:-100,
+    delay :7,
+    duration :1,
+    opacity :0,
+})
+gsap.from(".p5c-left button" ,{
+    y:-100,
+    delay :7,
+    duration :1,
+    opacity :0,
+})
